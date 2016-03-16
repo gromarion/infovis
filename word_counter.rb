57b1f01aa49254651c13092d091b4a3857ec6939
German Rodrigo Romarion <grromarion90@gmail.com>
2016-03-16 11:56:04 -0300

    Words are being sorted descending by their frequency

--- word_counter.rb
+++ word_counter.rb
@@ -24,24 +24,31 @@ end
 
 blacklisted_words = %w[of at the from in is for or and it to a are as an not can be use has which such on eg]
 ans = {}
+file_name = ARGV.shift
 
-File.open(ARGV.shift).each do |line|
-  line.split(' ').each do |word|
+words = File.open(file_name).flat_map do |line|
+  line.split(' ').map do |word|
     word = clear_word(word)
 
     next if blacklisted_words.include?(word)
-    if ans[word]
-      ans[word] += 1
-    else
-      ans[word] = 1
-    end
+    word
+  end
+end.compact
+
+words.each do |word|
+  if ans[word]
+    ans[word] += 1
+  else
+    ans[word] = 1
   end
 end
 
 target = File.open('ans.txt', 'w')
 
-formatted_ans = ans.map do |k, v|
-  { word: k, frequency: v }.to_json
-end
+formatted_ans = ans.map { |k, v| { word: k, frequency: v } }
+
+formatted_ans = formatted_ans.sort { |a, b| b[:frequency] <=> a[:frequency] }
+
+formatted_ans = formatted_ans.map { |data| { word: data[:word], frequency: data[:frequency] }.to_json }
 
 target.write(formatted_ans.to_s.delete('\\').gsub('"{', '{').gsub('}"', '}'))
